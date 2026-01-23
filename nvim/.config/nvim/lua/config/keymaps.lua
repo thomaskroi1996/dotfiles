e62c89f70ccea747860a2a816884762f4f3a8fc0
Thomas Kroissenbrunner <thomaskroi1996@gmail.com>
2026-01-23 13:17:40 +0100

    tmux, nvim, bash

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set('n', '<CR>', 'o<Esc>k')
+
 vim.keymap.set('n', '<S-CR>', 'O<Esc>j')
 vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<cr>")
--- Move around in Insert Mode
+
+-- split line with S
+vim.keymap.set('n', 'S', 'i<CR><Esc>')
+
+-- move around in insert mode
 vim.keymap.set('i', '<C-h>', '<Left>')
 vim.keymap.set('i', '<C-j>', '<Down>')
 vim.keymap.set('i', '<C-k>', '<Up>')
 vim.keymap.set('i', '<C-l>', '<Right>')
 
--- Jump words in Insert Mode
+-- jump words in insert mode
 vim.keymap.set('i', '<C-b>', '<C-o>b')
 vim.keymap.set('i', '<C-w>', '<C-o>w')
--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -1,48 +1,107 @@
 return {
   "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+  },
   config = function()
     local dap = require("dap")
+    local dapui = require("dapui")
 
-    -- 1. Keymaps
+    dapui.setup()
+
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+
     vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
     vim.keymap.set('n', '<Leader>dc', dap.continue, {})
 
-    -- 2. Adapter Configuration
-    dap.adapters.gdb = {
-      type = "executable",
-      command = "gdb",
-      args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+    vim.keymap.set('n', '<F10>', dap.step_over, {})
+    vim.keymap.set('n', '<F11>', dap.step_into, {})
+    vim.keymap.set('n', '<F12>', dap.step_out, {})
+
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = "${port}",
+      executable = {
+        -- This path matches the one we just cleaned/reinstalled
+        command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
+        args = { "--port", "${port}" },
+      }
     }
 
-    -- 3. Language Configurations
-    local config = {
+    dap.configurations.cpp = {
       {
-        name = "Launch",
-        type = "gdb",
+        name = "Launch file",
+        type = "codelldb", -- Must match the adapter name above
         request = "launch",
         program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'file')
         end,
-        cwd = "${workspaceFolder}",
-        stopAtBeginningOfMainSubprogram = false,
-      },
-      {
-        name = "Select and attach to process",
-        type = "gdb",
-        request = "attach",
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        pid = function()
-          local name = vim.fn.input('Executable name (filter): ')
-          return require("dap.utils").pick_process({ filter = name })
-        end,
-        cwd = '${workspaceFolder}'
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
       },
     }
-
-    dap.configurations.c = config
-    dap.configurations.cpp = config
-    dap.configurations.rust = config
   end,
 }
+-- return {
+--   "mfussenegger/nvim-dap",
+--   dependencies = {
+--     "rcarriga/nvim-dap-ui",
+--     "nvim-neotest/nvim-nio",
+--   },
+--   config = function()
+--     local dap = require("dap")
+--     local dapui = require("dapui")
+--
+--     dapui.setup()
+--
+--     dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+--     dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+--     dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+--
+--     vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, {})
+--     vim.keymap.set('n', '<Leader>dc', dap.continue, {})
+--
+--     vim.keymap.set('n', '<F10>', dap.step_over, {})
+--     vim.keymap.set('n', '<F11>', dap.step_into, {})
+--     vim.keymap.set('n', '<F12>', dap.step_out, {})
+--
+--     dap.adapters.gdb = {
+--       type = "executable",
+--       command = "gdb",
+--       args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+--     }
+--
+--     local config = {
+--       {
+--         name = "Launch",
+--         type = "gdb",
+--         request = "launch",
+--         program = function()
+--           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--         end,
+--         cwd = "${workspaceFolder}",
+--         stopAtBeginningOfMainSubprogram = false,
+--       },
+--       {
+--         name = "Select and attach to process",
+--         type = "gdb",
+--         request = "attach",
+--         program = function()
+--           return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--         end,
+--         pid = function()
+--           local name = vim.fn.input('Executable name (filter): ')
+--           return require("dap.utils").pick_process({ filter = name })
+--         end,
+--         cwd = '${workspaceFolder}'
+--       },
+--     }
+--
+--     dap.configurations.c = config
+--     dap.configurations.cpp = config
+--     dap.configurations.rust = config
+--   end,
+-- }
